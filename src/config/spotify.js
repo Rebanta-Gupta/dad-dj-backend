9f3f30e2d340c60fba84b6818cbef40ee5baa148
Rebanta Gupta <guptarebanta816@gmail.com>
2026-06-08 23:21:24 -0700

    Removes private playlist creation

--- src/config/spotify.js
+++ src/config/spotify.js
@@ -2,7 +2,7 @@ export const spotifyConfig = {
   authorizeUrl: "https://accounts.spotify.com/authorize",
   tokenUrl: "https://accounts.spotify.com/api/token",
   scopes: [
-    "playlist-modify-private",
+    //"playlist-modify-private",
     "playlist-modify-public",
     "user-top-read"
   ]
